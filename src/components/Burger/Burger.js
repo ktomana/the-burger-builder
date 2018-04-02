a45df8f80f56a309e4bf2aa87cbae9b8b76868e9
Klaudia Tomana <klaudia.tomana@gmail.com>
2018-04-02 13:45:20 +0200

    Add query params

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,10 +1,8 @@
 import React from 'react';
-
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  console.log(props);
   const transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])]
