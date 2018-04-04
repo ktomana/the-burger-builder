354c8ece10138ba81363b5f10db435934becf18b
Klaudia Tomana <klaudia.tomana@gmail.com>
2018-04-04 15:41:01 +0200

    Improve orders page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -44,7 +44,7 @@ class Checkout extends Component {
           checkoutContinued={this.checkoutContinuedHandler}/>
         <Route
           path={this.props.match.path + '/contact-data'}
-          render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.price} {...props}/>)} />
+          render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props}/>)} />
       </div>
     );
   }
